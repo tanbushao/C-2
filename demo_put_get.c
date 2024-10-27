e0c3540dd18d68f47e434edfbf31cec9e93bef3f
tanbushao <18752027775@163.com>
2024-10-27 20:40:40 +0800

    ninth commit

---
+++ demo_put_get.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+/*
+    跟printf的区别
+    1.puts会自动加入换行,而printf要加入\n进行换行
+    2.printf支持多种花样输出，而puts就是输出字符串
+*/
+
+    char c;
+
+    puts("请输入一个字符");
+    c = getchar();
+    puts("你输入的字符是");
+    getchar(c);
+
+
+    return 0;
+}
