451b7c784046ecc1ecae8f2dd4e5c59040264a09
tanbushao <18752027775@163.com>
2024-10-27 20:59:28 +0800

    eleventh commit

--- demo_put_get.c
+++ demo_put_get.c
@@ -6,13 +6,10 @@ int main()
     1.puts会自动加入换行,而printf要加入\n进行换行
     2.printf支持多种花样输出，而puts就是输出字符串
 */
-
     char c;
-
     puts("请输入一个字符");
     c = getchar();
     puts("你输入的字符是");
     putchar(c);
-
     return 0;
 }
